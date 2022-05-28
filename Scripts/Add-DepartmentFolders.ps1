35ac20a4cfd86644b436599a07ee969160e4e232
Kevin Richard <krichard1985@gmail.com>
2022-05-28 13:52:17 -0500

    Added new files. Fixed references in Add-DepartmentFolders

---
+++ Scripts/Add-DepartmentFolders.ps1
@@ -0,0 +1,48 @@
+﻿<#	
+.SYNOPSIS
+	Builds a Department folder for each User Group in an OU
+
+.DESCRIPTION
+	This script will check Active Directory for a list of groups, then check a share path to see if the folder exists. If not, it will create one.
+
+.PARAMETER Searchbase
+	The OU Path to search for AD Groups
+
+.PARAMETER DeptFolderPath
+	The UNC Share path for the Department folders.
+
+
+#>
+
+params(
+	[Parameter(Mandatory)]
+	[string]$DeptOU,
+
+	[Parameter(Mandatory)]
+	[string]$DeptFolderPath
+)
+$ADGroups = Get-ADGroup -filter * -searchbase $DeptOU
+
+foreach ($Group in $ADGroups) {
+	if ((Test-Path "$DeptFolderPath\$($group.name)") -eq $false) {
+		New-Item -Name $group.name -Path $DeptFolderPath -ItemType 'Directory'
+		
+		$FolderPath = "$DeptFolderPath\$($Group.name)"
+		$ACLGroup = $Group.name
+		$Rights = 'ReadandExecute', 'Traverse', 'Write'
+		$Inheritance = 'ContainerInherit,ObjectInherit'
+		$Propagation = 'None'
+		$ACERule = New-Object System.Security.AccessControl.FileSystemAccessRule($ACLGroup, $Rights, $Inheritance, $Propagation, 'Allow')
+		$acl = Get-Acl $FolderPath
+		$acl.SetAccessRule($ACERule)
+		
+		$acl | Set-Acl $FolderPath
+		
+		$NewFolders += "$($Group.name)`n"
+	}
+	
+}
+Write-Output $NewFolders
+
+
+
---
+++ Scripts/Add-HomeFolder.ps1
@@ -0,0 +1,45 @@
+﻿<#	
+	.SYNOPSIS
+		Creates user folder for each user in an Active Directory OU
+	
+	.DESCRIPTION
+		This script will query AD for a list of users and then check if the folder exists on the share. If not, it will create one with the approriate permissions
+
+	.PARAMETER UserDirectory
+		The UNC Path for the User folders
+
+	.PARAMETER UserOU
+		The OU for the user accounts
+
+	.Parameter LookBackDays
+		The amount of days to look back for when a user was created. This is to prevent a large list of users being returned.
+#>
+
+param(
+	[Parameter(Mandatory)]
+	[string]$UserOU,
+
+	[Parameter(Mandatory)]
+	[string]$UserDirectory,
+
+	[int]$LookBackDays = 3650
+)
+
+$date = (Get-Date).AddDays(-$LookBackDays)
+$Users = get-aduser -filter { (enabled -eq 'true') -and (whencreated -ge $date) } -searchbase $UserOU
+
+foreach ($user in $users) {
+	$UserFolderPath = "$UserDirectory\$($User.samaccountname)"
+
+	if ((Test-Path $UserFolderPath ) -eq $false) {
+		New-Item -Name $user.samaccountname -ItemType 'Directory' -Path $UserDirectory
+		$ACLGroup = $user.samaccountname
+		$Rights = 'FullControl'
+		$Inheritance = 'ContainerInherit,ObjectInherit'
+		$Propagation = 'None'
+		$ACERule = New-Object System.Security.AccessControl.FileSystemAccessRule($ACLGroup, $Rights, $Inheritance, $Propagation, 'Allow')
+		$acl = Get-Acl $UserFolderPath
+		$acl.SetAccessRule($ACERule)
+		$acl | Set-Acl $UserFolderPath
+	}
+}
