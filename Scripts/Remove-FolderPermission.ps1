a8bd06dd51d57dbe41dbb6ecb690f7fe577118f4
Kevin Richard <krichard1985@gmail.com>
2022-05-28 13:02:13 -0500

    Moved scripts to Scripts folder

---
+++ Scripts/Remove-FolderPermission.ps1
@@ -0,0 +1,99 @@
+﻿<#
+	.SYNOPSIS
+		Removes Access for a User or Group from a Folder
+
+	.DESCRIPTION
+		This command will remove a user or group ACL from a folder.
+
+	.PARAMETER  FolderPath
+		The UNC Path to the folder to remove permissions
+
+	.PARAMETER  Groups
+		The Users or Groups that need to be removed. Can be an array of users or groups
+
+	.PARAMETER  LogFailuresToPath
+		Log Folder Path of Failed Removals
+
+	.EXAMPLE
+		Remove-FolderPermission -FolderPath $Path -Groups $IdentityToRemove
+
+	.EXAMPLE
+		Import-CSV $FoldersToRemove | Remove-FolderPermission -Groups $IdentityToRemove
+
+	.NOTES
+		Additional information about the function go here.
+
+	.LINK
+		about_functions_advanced
+
+	.LINK
+		about_comment_based_help
+
+#>
+
+function Remove-FolderPermission
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true,
+				   ValueFromPipelineByPropertyName = $true)]
+		[System.String]$FolderPath,
+		
+		[Parameter(Mandatory = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromPipeline = $true)]
+		[System.String[]]$Group,
+		
+		[string]$LogFailuresToPath
+		
+	)
+	
+	BEGIN { }
+	
+	PROCESS
+	{
+		try
+		{
+			Write-Verbose 'Checking if Folder Path is valid'
+			if (-not (Test-Path $FolderPath))
+			{
+				Write-Error "Folder path does not exist"
+			}
+			foreach ($Identity in $group)
+			{
+				Write-Verbose 'Removing $Identity from $FolderPath'
+				
+				$acl = Get-Acl $FolderPath
+				$ACERule = New-Object System.Security.AccessControl.FilesystemAccessRule($Identity,'Read',,,'Allow')
+				$acl.RemoveAccessRuleAll($ACERule)
+				$acl | Set-Acl $FolderPath
+				
+				$props = @{
+					'Group' = $Identity
+					'Path' = $FolderPath
+				}
+				
+				$obj = New-Object -TypeName System.Management.Automation.PSObject -Property $props
+				
+				Write-Output $obj
+			} #FOREACH
+			
+		} #TRY
+		
+		catch
+		{
+			if ($PSBoundParameters.ContainsKey('LogFailuresToPath'))
+			{
+				Write-Verbose "Logging to $LogFailuresToPath"
+				$FolderPath | Out-File -FilePath $LogFailuresToPath -Encoding ascii -Append
+			}
+			else
+			{
+				Write-Verbose "Passing error to console"
+				Write-Error $_
+			}
+		} #CATCH
+	} #PROCESS
+	
+	END { }
+} #FUNCTION
---
+++ Scripts/Remove-StaleDHCPLeases.ps1
@@ -0,0 +1,63 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.174
+	 Created on:   	4/12/2021 8:17 AM
+	 Created by:   	Kevin Richard
+	 Organization: 	
+	 Filename:     	Remove-StaleDHCPLeases
+	===========================================================================
+	.DESCRIPTION
+		Pings a scope and removes the IP Lease if there is no reply. This script assumes you can ping all endpoints
+		in the scope.
+#>
+
+
+param (
+	
+	[string]$DHCPServer,
+	
+	[pscredential]$Credential,
+	
+	[string]$Scope,
+	
+	[switch]$DisplayStaleLeases
+)
+
+#Splatting parameters for Test-Connection
+$TestConnectionParams = @{
+	Quiet = $true
+	Count = 1
+}
+
+#Checks if Powershell 7 to import dhcp module and add extra functions specific to PS7 version of Test-Connection
+if ($PSVersionTable.psversion -like "7.*"){
+	Import-Module DhcpServer -ErrorAction Stop -skipeditioncheck
+	$TestConnectionParams.TimeoutSeconds = 1
+}
+
+$DHCPCimSession = New-CimSession -Credential $Credential -ComputerName $DHCPServer
+
+$Leases = Get-DhcpServerv4Lease -Cimsession $DHCPCimSession -ScopeId $Scope
+
+$StaleLeases = @()
+
+$Count = 1
+foreach ($Lease in $Leases)
+{
+	$Count ++ 
+	Write-Progress -Activity 'Deleting Stale Leases' -PercentComplete ($Count/$Leases.Count * 100)
+	$Connected = Test-Connection -ComputerName $Lease.ipaddress @TestConnectionParams
+	
+	if ($Connected -eq $false)
+	{
+		$StaleLeases += $Lease
+		Remove-DhcpServerv4Lease -CimSession $DHCPCimSession -IPAddress $Lease.ipaddress
+	}
+} #FOREACH 
+Write-Output "Removed $($StaleLeases.count)"
+
+if ($DisplayStaleLeases)
+{
+	$StaleLeases | Out-GridView
+}
