a82bea3494d58b6393b19cf232544f441f38bbea
Kevin Richard <krichard1985@gmail.com>
2022-05-29 11:30:43 -0500

    Added new files

---
+++ Scripts/Remove-FilesbyDate.ps1
@@ -0,0 +1,17 @@
+﻿<#	
+	.DESCRIPTION
+		This script will remove files based on How old and the file type. 
+#>
+param (
+	[Parameter(Mandatory)]
+	[int]$OlderThanDays = 90,
+
+	[Parameter(Mandatory)]
+	[string]$Path,
+	
+	[string]$FileType = '.*'
+)
+
+$TargetDate = (Get-Date).AddDays(-$OlderThanDays)
+$FilesToDelete = Get-ChildItem -Path $Path | Where-Object -FilterScript { $_.CreationTime -le $TargetDate -and $_.Extension -like $FileType }
+$FilesToDelete | Remove-Item -Confirm:$false
